191ec79467a7bd2bd96bd14d4ffc916801258263
ebruesraustuner <ebruesra06@gmail.com>
2020-12-11 13:34:50 +0300

    Update Contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,5 @@
 <template>
-        <form class="form" name="contactForm" id="contactForm"  method="post" data-netlify="true" @submit="contactForm" novalidate="true">
+        <form class="form" name="contactForm"  method="post" data-netlify="true">
 
             <p v-if="errors.length">
                 <b>Please correct the following error(s):</b>
@@ -38,30 +38,7 @@
             }
         },
         methods: {
-            contactForm: function (e) {
-                if (this.name && this.email) {
-                    return true;
-                }
-                this.errors = [];
-                if (!this.name) {
-                    this.errors.push('Name Required');
-                }
-                if (!this.email) {
-                    this.errors.push('Email required.');
-                } else if (!this.validEmail(this.email)) {
-                    this.errors.push('Valid email required.');
-                }
-                if (!this.errors.length) {
-                    return true;
-                }
 
-                e.preventDefault();
-            },
-            validEmail: function (email) {
-                var re =
-                    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                return re.test(email);
-            }
 
         }
 
